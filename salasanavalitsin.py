6bc9681487d705cc79fccf9de9f360fd8dd39616
Jukka Prami <77621938+jukkaprami@users.noreply.github.com>
2023-03-28 14:11:15 +0300

    1

---
+++ salasanavalitsin.py
@@ -0,0 +1,8 @@
+Import random
+
+password = ('omena', 'dinosaurus', 'pallo', 'paahdin', 'vuohi', 'lohikäärme', 'vasara', 'sorsa', 'panda', 'puhelin', 'banaani', 'opettaja')
+password = ('uninen', 'hidas', 'haiseva', 'märkä', 'lihava', 'punainen', 'oranssi', 'keltainen', 'vihreä', 'sininen', 'purppura', 'pörröinen', 'valkoinen', 'leuhka', 'uljas')
+
+while True:
+    # valitaan satunnaisesti jonkin adjectives listalta
+    adjective = password
