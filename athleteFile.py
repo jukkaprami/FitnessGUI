d3e5897aa95d584587f0ef2fbe7afcd908ac08d8
Jukka Prami <77621938+jukkaprami@users.noreply.github.com>
2023-04-21 10:08:26 +0300

    aaaa

---
+++ athleteFile.py
@@ -0,0 +1,37 @@
+# ROTINES TO CREATE, WRITE AND READ ATHLETE DATA FUKE
+# ===================================================
+
+# LIBRARIES AND MODULES
+
+import json
+
+# CLASS DEFINITIONS
+
+class ProcessJsonFile(object):
+    def __init__(self):
+        pass
+
+    def saveData(self,file):
+        """_summary_
+
+        Args:
+            file (str): Name of the file
+            data (list): List of dictionaries
+
+        Returns:
+            _type_: error code, error message, detailed error message
+        """
+        status = (0, 'Tallennus onnistui', 'All data saved succesfully')
+        return status
+
+    def readData(self,arg):
+        pass
+
+
+    def appenData(self, arg):
+        pass
+
+# PRELIMINARY TESTS
+
+if __name__ == "__main__":
+    pass
