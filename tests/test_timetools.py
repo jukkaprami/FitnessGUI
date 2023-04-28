4caa1f9cd135f1b2ac9336565bfd16aa8f31bcf4
Jukka Prami <77621938+jukkaprami@users.noreply.github.com>
2023-04-28 10:46:22 +0300

    st

--- tests/test_timetools.py
+++ tests/test_timetools.py
@@ -5,6 +5,12 @@ import timetools
 
 # UNIT TESTS DEFINITIONS
 
+# Test if datediff function calculates correct and absolute values
 def test_datediff():
     assert timetools.datediff('2023-04-28', '2023-04-10') == 18
-    
+    assert timetools.datediff('2023-04-10', '2023-04-28') == 18
+
+# Test if datediff function calculates correct and absolute values
+def test_timediff():
+    assert timetools.timediff('11:30:15', '10:10:05') == 1,3361111111111
+    assert timetools.timediff('10:10:05', '11:30:15') == 1,3361111111111
